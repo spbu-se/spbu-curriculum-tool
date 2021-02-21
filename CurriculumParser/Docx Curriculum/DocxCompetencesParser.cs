3f4b290e403e4497a5a1ba7914bcaae7d03e3e49
resueman <44446084+resueman@users.noreply.github.com>
2021-02-21 10:56:17 +0300

    исправлена ошибка парсинга компетенций при нестандартном кол-ве столбцов в таблице компетенций

--- CurriculumParser/Docx Curriculum/DocxCompetencesParser.cs
+++ CurriculumParser/Docx Curriculum/DocxCompetencesParser.cs
@@ -20,11 +20,12 @@ namespace CurriculumParser
             try
             {
                 var competences = new List<Competence>();
-                var table = body.Elements<Table>().Skip(2).First();
+                var table = body.Elements<Table>().Skip(2).First();                
+                var (codeCellIndex, descriptionCellIndex) = AnalyzeCompetenceTableHeader(table.Elements<TableRow>().ElementAt(0));                
                 foreach (var row in table.Elements<TableRow>().Skip(1))
                 {
-                    var code = row.Elements<TableCell>().ElementAt(0).InnerText;
-                    var descriptionRuns = row.Elements<TableCell>().ElementAt(1).Descendants<Run>();
+                    var code = row.Elements<TableCell>().ElementAt(codeCellIndex).InnerText;
+                    var descriptionRuns = row.Elements<TableCell>().ElementAt(descriptionCellIndex).Descendants<Run>();
                     var description = "";
                     foreach (var run in descriptionRuns)
                     {
@@ -47,6 +48,19 @@ namespace CurriculumParser
             {
                 throw new CurriculumParsingException("Ошибка парсинга компетенций .docx", e);
             }
+        }
+
+        private static (int codeCellIndex, int descriptionCellIndex) AnalyzeCompetenceTableHeader(TableRow row)
+        {
+            var codeCell = row.Elements<TableCell>().Single(c => c.InnerText.ToLower().Contains("код"));
+            var descriptionCell = row.Elements<TableCell>().Single(c => c.InnerText.ToLower().Contains("описание компетенции") 
+                || c.InnerText.ToLower().Contains("наименование компетенции"));
+            
+            var cells = row.Elements<TableCell>().ToList().ToList();
+            var codeCellIndex = cells.IndexOf(codeCell);
+            var descriptionCellIndex = cells.IndexOf(descriptionCell);
+
+            return (codeCellIndex, descriptionCellIndex);
         }
     }
 }
